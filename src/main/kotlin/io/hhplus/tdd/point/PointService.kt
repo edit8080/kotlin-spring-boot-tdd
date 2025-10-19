70ce073193b85aa36f5acc1a4ce878e94b763501
edit.re <edit.re@kakaohealthcare.com>
2025-10-19 21:04:12 +0900

    feat: 사용자별 포인트 충전 로직에 동시성 제어

--- src/main/kotlin/io/hhplus/tdd/point/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -2,30 +2,40 @@ package io.hhplus.tdd.point
 
 import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class PointService(
     private val userPointTable: UserPointTable,
     private val pointHistoryTable: PointHistoryTable
 ) {
+    // 사용자별 락 저장소 선언
+    private val userLocks = ConcurrentHashMap<Long, ReentrantLock>()
+
     // 포인트 충전
     fun chargePoint(userId: Long, amount: Long): UserPoint {
         if(amount <= 0){
             throw PointException.InvalidPointAmount(amount);
         }
 
-        val userPoint = userPointTable.selectById(userId)
-        val updatedPointAmount = userPoint.point + amount;
-        val updatedUserPoint = userPointTable.insertOrUpdate(userId, updatedPointAmount)
+        val lock = userLocks.computeIfAbsent(userId) { ReentrantLock() }
 
-        // 포인트 충전 이력 저장
-        pointHistoryTable.insert(
-            userId,
-            amount,
-            TransactionType.CHARGE,
-            updatedUserPoint.updateMillis
-        )
+        lock.withLock {
+            val userPoint = userPointTable.selectById(userId)
+            val updatedPointAmount = userPoint.point + amount;
+            val updatedUserPoint = userPointTable.insertOrUpdate(userId, updatedPointAmount)
 
-        return updatedUserPoint
+            // 포인트 충전 이력 저장
+            pointHistoryTable.insert(
+                userId,
+                amount,
+                TransactionType.CHARGE,
+                updatedUserPoint.updateMillis
+            )
+
+            return updatedUserPoint
+        }
     }
 
     // 포인트 사용
